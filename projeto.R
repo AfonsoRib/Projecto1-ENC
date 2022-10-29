77638000f06113ce0609305dfc16280c38c7d308
AfonsoRib <afonsoviegas0@gmail.com>
2022-10-29 21:45:33 +0100

    ADD algumas explicações do exercicios

--- projeto.R
+++ projeto.R
@@ -2,6 +2,7 @@ pdf = function(x, alpha, L,H){
     (alpha * L^(alpha) * x^(-alpha-1))/(1 - (L/H)^(alpha))
 }
 
+## (b)
 sim.IT = function(n,alpha, L, H){
     x=0
     t=0
@@ -16,13 +17,16 @@ sim.IT = function(n,alpha, L, H){
     x    
 }
 
-## por enquanto g=1 (função constante). Podemos arranjar uma distribuição exponencial para otimizar os resultados
+## (e)
 sim.AR = function(n,alpha, L,H){
     f =function (x){ (alpha * L^(alpha) * x^(-alpha-1))/(1 - (L/H)^(alpha)) }
     g = function(x){exp(-x)}
     h = function(x){f(x)/g(x)}
-    M = optimize(h, c(L,H),maximum = T)$objective
-
+    M = optimize(h, c(L,H),maximum = T)
+    curve(h)
+    print(M)
+    print(h(3))
+    M = M$objective
     g.CDF=function(x){-exp(-x)+1}
     g.ITM = function(x){-log(1-x)}
     v = vector()
@@ -40,42 +44,35 @@ sim.AR = function(n,alpha, L,H){
 }
 
 
-#(d) Since 15 samples is too small to make any conclusion, we cannot conclude that the code is not generating samples correctly. We can see that it has more samples in the beginning than in the end as we want so it can be likely that it is generating samples from the assumed distribution altough not conclusivly
-
-library(arm)
-library(Cairo)
+## (d) Since 15 samples is too small to make any conclusion, we cannot conclude that the code is not generating samples correctly. We can see that it has more samples in the beginning than in the end as we want so it can be likely that it is generating samples from the assumed distribution altough not conclusivly
+## (g)
 set.seed(2447)
+n=12000
 alpha=0.25
 L=2
 H=3
-set.IT = sim.IT(12000,alpha,L,H)
-set.AR = sim.AR(12000,alpha,L,H)
-gTest = function(x,lambda){ lambda*exp(-lambda*x)}
-par(mfrow=c(1,2))
-hist (set.IT[1:15],main="ITM Truncated Pareto",xlim=c(L,H),freq = F)
-curve(pdf(x,alpha,L,H) ,add = T, xlim=c(L,H))    
-hist(set.AR[1:15],main="ARM Truncated Pareto",xlim=c(L,H),freq = F)
-curve(pdf(x,alpha,L,H) ,add = T, xlim=c(L,H))
-
-set.seed(2447)
-alpha=0.25
-L=2
-H=3
-set.IT = sim.IT(12000,alpha,L,H)
-set.AR = sim.AR(12000,alpha,L,H)
+set.IT = sim.IT(n,alpha,L,H)
+set.AR = sim.AR(n,alpha,L,H)
 par(mfrow=c(1,2))
 hist (set.IT,main="ITM Truncated Pareto",xlim=c(L,H),freq = F)
-curve(pdf(x,alpha,L,H) ,add = T, xlim=c(2,4))
+curve(pdf(x,alpha,L,H) ,add = T, xlim=c(L,H))
 hist(set.AR,main="ARM Truncated Pareto", xlim=c(L,H),freq = F)
-curve(pdf(x,alpha,L,H) ,add = T, xlim=c(2,4))    
-curve(1.2967*1+(x-x),add=T,xlim=c(2,4))
+curve(pdf(x,alpha,L,H) ,add = T, xlim=c(L,H))    
 
-
+##
 library(Pareto)
 par(mfrow=c(1,2))
 hist (set.IT,main="ITM Truncated Pareto",xlim=c(L,H),freq = F)
-curve(pdf(x,alpha,L,H) ,add = T, xlim=c(2,4))
+curve(pdf(x,alpha,L,H) ,add = T, xlim=c(L,H))
 hist (rPareto(12000,2,0.25,truncation=3),main="ITM Truncated Pareto",xlim=c(L,H),freq = F)
-curve(pdf(x,alpha,L,H) ,add = T, xlim=c(2,4))
+curve(pdf(x,alpha,L,H) ,add = T, xlim=c(L,H))
 
 
+## (f) d/dx h(x) = d/dx (0.25 * 2^(0.25) * x^(-1.25))/(1 - (2/3)^(0.25))/(e^-x) =
+##     e^x (-3.85513/x^2.25 + 3.08411/x^1.25)
+##     e^x (-3.85513/x^2.25 + 3.08411/x^1.25) = 0 => x ~= 1.25
+##     Derivada positiva a partir de 1.25 logo está a crescer.
+##     Queremos maior valor no intervalo [2,3]
+##     Como a derivada só cresce o maior valor estará no 3
+##     h(3) = 15.68958 <=> M = 15.68958
+##     alpha = (1/M)h(x)

eb87ab8ef5ee050aa726d493852310fcf3234896
AfonsoRib <afonsoviegas0@gmail.com>
2022-10-30 12:26:45 +0000

    ADD 1.1 feito

--- projeto.R
+++ projeto.R
@@ -1,9 +1,16 @@
+####################################################
+##                        1.1                     ##
+####################################################
+
 pdf = function(x, alpha, L,H){
     (alpha * L^(alpha) * x^(-alpha-1))/(1 - (L/H)^(alpha))
 }
 
 ## (b)
 sim.IT = function(n,alpha, L, H){
+    if(L>H) stop("Lower bound greater than Higher bound")
+    else if(alpha <= 0) stop("Alpha must be a positive number")
+    else if(L <= 0) stop("Lower Bound must be a positive number")
     x=0
     t=0
     while(t<n){
@@ -19,29 +26,51 @@ sim.IT = function(n,alpha, L, H){
 
 ## (e)
 sim.AR = function(n,alpha, L,H){
+    if(L>H) stop("Lower bound greater than Higher bound")
+    else if(alpha <= 0) stop("Alpha must be a positive number")
+    else if(L <= 0) stop("Lower Bound must be a positive number")
+    x = vector()
+    x_rej = vector()
+    y = vector()
+    y_rej = vector()
     f =function (x){ (alpha * L^(alpha) * x^(-alpha-1))/(1 - (L/H)^(alpha)) }
     g = function(x){exp(-x)}
     h = function(x){f(x)/g(x)}
-    M = optimize(h, c(L,H),maximum = T)
-    curve(h)
-    print(M)
-    print(h(3))
-    M = M$objective
+    M = optimize(h, c(L,H),maximum = T)$objective
     g.CDF=function(x){-exp(-x)+1}
     g.ITM = function(x){-log(1-x)}
-    v = vector()
     for(i in 1:n){
         u <- 1
         a <- 0
         while(u> a){
             x.c   <- g.ITM(runif(1,g.CDF(2),g.CDF(3))) # O g.CDF aqui serve para não estar a gerar valores desnecessários através da distribuição exponencial
             a <- f(x.c)/(M*g(x.c)) 
-            u     <- runif(1,0,1)          
+            u <- runif(1,0,1)
+            if(u > a) {
+                x_rej = c(x_rej, x.c )
+                y_rej = c(y_rej, u*M*g(x.c))
+            }
         }
-        v <- c(v,x.c)
+        x <- c(x,x.c)
+        y <- c(y,u*M*g(x.c))
     }
-    v
+    list(x = x,x_rej = x_rej,y = y, y_rej =y_rej)
 }
+
+## plot with rejected point and accepted points
+alpha = 0.25
+L = 2
+H = 3
+f =function (x){ (alpha * L^(alpha) * x^(-alpha-1))/(1 - (L/H)^(alpha)) }
+g = function(x){exp(-x)}
+h = function(x){f(x)/g(x)}
+M = optimize(h, c(L,H),maximum = T)$objective
+Mg= function(x){M*g(x)}
+samples = sim.AR(12000,alpha,L,H)
+plot(f,add=T,xlim=c(L,H),col="green")
+curve(Mg,add=T,xlim=c(L,H))
+points(samples$x,samples$y,pch=4,cex=1,lwd=1.5)
+points(samples$x_rej,samples$y_rej,col=2,pch=4,cex=1,lwd=1.5)
 
 
 ## (d) Since 15 samples is too small to make any conclusion, we cannot conclude that the code is not generating samples correctly. We can see that it has more samples in the beginning than in the end as we want so it can be likely that it is generating samples from the assumed distribution altough not conclusivly
@@ -52,21 +81,12 @@ alpha=0.25
 L=2
 H=3
 set.IT = sim.IT(n,alpha,L,H)
-set.AR = sim.AR(n,alpha,L,H)
+set.AR = sim.AR(n,alpha,L,H)$x
 par(mfrow=c(1,2))
 hist (set.IT,main="ITM Truncated Pareto",xlim=c(L,H),freq = F)
 curve(pdf(x,alpha,L,H) ,add = T, xlim=c(L,H))
 hist(set.AR,main="ARM Truncated Pareto", xlim=c(L,H),freq = F)
 curve(pdf(x,alpha,L,H) ,add = T, xlim=c(L,H))    
-
-##
-library(Pareto)
-par(mfrow=c(1,2))
-hist (set.IT,main="ITM Truncated Pareto",xlim=c(L,H),freq = F)
-curve(pdf(x,alpha,L,H) ,add = T, xlim=c(L,H))
-hist (rPareto(12000,2,0.25,truncation=3),main="ITM Truncated Pareto",xlim=c(L,H),freq = F)
-curve(pdf(x,alpha,L,H) ,add = T, xlim=c(L,H))
-
 
 ## (f) d/dx h(x) = d/dx (0.25 * 2^(0.25) * x^(-1.25))/(1 - (2/3)^(0.25))/(e^-x) =
 ##     e^x (-3.85513/x^2.25 + 3.08411/x^1.25)
@@ -76,3 +96,40 @@ curve(pdf(x,alpha,L,H) ,add = T, xlim=c(L,H))
 ##     Como a derivada só cresce o maior valor estará no 3
 ##     h(3) = 15.68958 <=> M = 15.68958
 ##     alpha = (1/M)h(x)
+
+## (h)
+compareTimes = function(m,nruns,alpha,L,H){
+    library(Pareto)
+    v.IT = vector()
+    v.AR = vector()
+    v.Pareto = vector()
+    i=0
+    while(i < nruns){
+        print(i)
+        ptm <- proc.time()
+        z = sim.IT(m,alpha,L,H)
+        v.IT = c(v.IT,proc.time() -ptm)
+        ptm <- proc.time()
+        z = sim.AR(m,alpha,L,H)
+        v.AR = c(v.AR,proc.time() -ptm)
+        ptm <- proc.time()
+        z=rPareto(m,L,alpha,truncation=H)
+        v.Pareto = c(v.Pareto,proc.time() -ptm)
+        i = i +1
+    }
+    printf <- function(...) invisible(print(sprintf(...)))
+    printf("ITM mean: %f", mean(v.IT))
+    printf("ITM standard deviation: %f", sd(v.IT))
+    printf("ARM mean: %f", mean(v.AR))
+    printf("ARM standard deviation: %f", sd(v.AR))
+    printf("built-in mean: %f", mean(v.Pareto))
+    printf("built-in standard deviation: %f", sd(v.Pareto))
+    
+}
+
+compareTimes(65000,2000,0.25,2,3)
+
+
+####################################################
+##                        1.2                     ##
+####################################################
